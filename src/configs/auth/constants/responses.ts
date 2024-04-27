9aed392c4d998605577474e1862c16f58c7ca110
KevnnardFedora <londonokevin9@gmail.com>
2024-04-26 20:13:18 -0500

    feat: add JWT constants and authentication responses

---
+++ src/configs/auth/constants/responses.ts
@@ -0,0 +1,6 @@
+export const RESPONSES = {
+  USER_NOT_FOUND: 'User not found, Please Sign Up',
+  INVALID_CREDENTIALS: 'Invalid Credentials, Please try again.',
+  USER_CREATED: 'User Created Successfully',
+  USER_ALREADY_EXISTS: 'User Already Exists, Choose another email',
+};
