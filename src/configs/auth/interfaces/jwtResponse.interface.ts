815fd0f77a58a4de8435673d1b572ecbee8034ba
KevnnardFedora <londonokevin9@gmail.com>
2024-04-26 20:18:50 -0500

    fix(auth): typo in JwtResponse interface

--- src/configs/auth/interfaces/jwtResponse.interface.ts
+++ src/configs/auth/interfaces/jwtResponse.interface.ts
@@ -3,7 +3,6 @@
  * @description Interface for the JWT response token and user
  * @property {string} token - The access token for the user
  * @returns { token: string }
- * @example { token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoiYWRtaW5AZ21haWwuY29tIiwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjI5MzIwMzIyLCJleHAiOjE2MjkzMjA0MjJ9.3Z'
  */
 export interface JwtResponse {
   /**
