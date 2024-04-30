bc283784a3e6719d664a51951be9beb090e757bd
KevnnardFedora <londonokevin9@gmail.com>
2024-04-30 00:57:33 -0500

    feat(http-service): httpsService to add postHttps method for making POST requests

--- src/modules/http/https.service.ts
+++ src/modules/http/https.service.ts
@@ -13,11 +13,30 @@ export class HttpsService {
         'X-Context': 'seller',
         Authorization: `Bearer ${httpBody.token}`,
       },
-      data: httpBody.data || null,
     };
 
     return await this.httpService.axiosRef
       .get(httpBody.url, config)
+      .then((response) => {
+        return response.data;
+      })
+      .catch((error) => {
+        throw new BadRequestException(error);
+      });
+  }
+
+  async postHttps(httpBody: HttpBodyTypes) {
+    const config = {
+      maxBodyLength: Infinity,
+      headers: {
+        accept: 'application/json',
+        'X-Context': 'seller',
+        Authorization: `Bearer ${httpBody.token}`,
+      },
+    };
+
+    return await this.httpService.axiosRef
+      .post(httpBody.url, httpBody.data, config)
       .then((response) => {
         return response.data;
       })
