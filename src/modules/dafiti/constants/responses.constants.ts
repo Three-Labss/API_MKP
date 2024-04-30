0c86bdf80679486d2c98f1071a22ab45fcd6bf9e
KevnnardFedora <londonokevin9@gmail.com>
2024-04-30 00:56:48 -0500

    refactor: update Dafiti module responses in responses.constants.ts

--- src/modules/dafiti/constants/responses.constants.ts
+++ src/modules/dafiti/constants/responses.constants.ts
@@ -1,13 +1,17 @@
 export const RESPONSES = {
   SUCCESS: 'Exito en la operación ',
   LIMIT: 'El limite no puede ser mayor a 50',
-  ERROR: 'ERROR',
-  NOT_FOUND: 'NOT_FOUND',
+  ERROR:
+    'Error en la operación, si el error persiste contacte al administrador del sistema',
+  NOT_FOUND: 'No se encontraron resultados para la búsqueda',
   UNAUTHORIZED:
     'Error en la autenticación, si el error persiste contacte al administrador del sistema',
-  FORBIDDEN: 'FORBIDDEN',
-  BAD_REQUEST: 'BAD_REQUEST',
-  CONFLICT: 'CONFLICT',
-  INTERNAL_SERVER_ERROR: 'INTERNAL_SERVER_ERROR',
-  SERVICE_UNAVAILABLE: 'SERVICE_UNAVAILABLE',
+  FORBIDDEN:
+    'No tiene permisos para realizar esta operación, contacte al administrador del sistema',
+  BAD_REQUEST: 'Solicitud incorrecta, verifique los datos enviados',
+  CONFLICT: 'Conflicto en la operación, verifique los datos enviados',
+  INTERNAL_SERVER_ERROR:
+    'Error interno del servidor, contacte al administrador del sistema',
+  SERVICE_UNAVAILABLE:
+    'Servicio no disponible, contacte al administrador del sistema',
 };
