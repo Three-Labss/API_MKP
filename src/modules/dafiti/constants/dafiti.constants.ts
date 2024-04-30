e7783cef93902c8f36a6ccf06019a1511972ea82
KevnnardFedora <londonokevin9@gmail.com>
2024-04-30 00:56:12 -0500

    feat(env): add DAFITI_URL_API_V1 constant to DAFITI module

--- src/modules/dafiti/constants/dafiti.constants.ts
+++ src/modules/dafiti/constants/dafiti.constants.ts
@@ -1,6 +1,7 @@
 export const DAFITI = {
   URL: process.env.DAFITI_URL,
   URL_API: process.env.DAFITI_URL_API,
+  URL_API_V1: process.env.DAFITI_URL_API_V1,
   CLIENT_ID: process.env.DAFITI_USER_APP_ID,
   CLIENT_SECRET: process.env.DAFITI_USER_APP_SECRET,
 };
