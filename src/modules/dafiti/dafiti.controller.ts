1c96511a20f1e07cb91932f2123c75d45ebd3bdf
KevnnardFedora <londonokevin9@gmail.com>
2024-04-30 00:55:51 -0500

    refactor: dafitiController to add new endpoints and update existing ones

--- src/modules/dafiti/dafiti.controller.ts
+++ src/modules/dafiti/dafiti.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
-import { ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param, Post, Query, UseGuards } from '@nestjs/common';
+import { ApiParam, ApiQuery, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { JwtClientGuard } from 'src/configs/auth/guards/client.guard';
 import { RESPONSES } from './constants';
 import { DafitiService } from './dafiti.service';
@@ -27,11 +27,38 @@ export class DafitiController {
     description: RESPONSES.LIMIT,
     links: {},
   })
+  @Get('orders/counters')
+  async getAllOrdersCounters(@DafitiAuth() token: string = '') {
+    return await this.dafitiService.ordersCounters({ token });
+  }
+
+  @ApiResponse({
+    status: 200,
+    description: RESPONSES.SUCCESS,
+    links: {},
+  })
+  @ApiResponse({
+    status: 401,
+    description: RESPONSES.UNAUTHORIZED,
+    links: {},
+  })
+  @ApiResponse({
+    status: 408,
+    description: RESPONSES.LIMIT,
+    links: {},
+  })
+  @ApiQuery({
+    name: 'limit',
+    type: 'string',
+    description: 'El limite maximo de ordenes',
+    example: '10',
+  })
   @Get('orders/pending')
   async getAllOrdersPending(
     @DafitiAuth() token: string = '',
-  ): Promise<{ totalOrdenesPendientes: number }> {
-    return await this.dafitiService.ordersPendingCount(token);
+    @Query('limit') limit: string = '10',
+  ) {
+    return await this.dafitiService.ordersPending({ token, limit });
   }
 
   @ApiResponse({
@@ -60,11 +87,138 @@ export class DafitiController {
     @DafitiAuth() token: string = '',
     @Query('limit') limit: string = '10',
   ) {
-    return await this.dafitiService.ordersReadyToShip(token, limit);
+    return await this.dafitiService.ordersReadyToShip({ token, limit });
   }
 
-  @Get('order/new')
-  async getNewOrder() {
-    return console.log('new order');
+  @ApiResponse({
+    status: 200,
+    description: RESPONSES.SUCCESS,
+    links: {},
+  })
+  @ApiResponse({
+    status: 401,
+    description: RESPONSES.UNAUTHORIZED,
+    links: {},
+  })
+  @ApiResponse({
+    status: 408,
+    description: RESPONSES.LIMIT,
+    links: {},
+  })
+  @Post('orders/ready-to-ship')
+  async setOrdersReadyToShip(@DafitiAuth() token: string = '') {
+    return await this.dafitiService.setOrdersReadyToShip({ token });
+  }
+
+  @ApiResponse({
+    status: 200,
+    description: RESPONSES.SUCCESS,
+    links: {},
+  })
+  @ApiResponse({
+    status: 401,
+    description: RESPONSES.UNAUTHORIZED,
+    links: {},
+  })
+  @ApiResponse({
+    status: 408,
+    description: RESPONSES.LIMIT,
+    links: {},
+  })
+  @ApiQuery({
+    name: 'limit',
+    type: 'string',
+    description: 'El limite maximo de ordenes',
+    example: '10',
+  })
+  @Get('orders/shipped')
+  async getAllOrdersShipped(
+    @DafitiAuth() token: string = '',
+    @Query('limit') limit: string = '10',
+  ) {
+    return await this.dafitiService.ordersShipped({ token, limit });
+  }
+
+  @ApiResponse({
+    status: 200,
+    description: RESPONSES.SUCCESS,
+    links: {},
+  })
+  @ApiResponse({
+    status: 401,
+    description: RESPONSES.UNAUTHORIZED,
+    links: {},
+  })
+  @ApiResponse({
+    status: 408,
+    description: RESPONSES.LIMIT,
+    links: {},
+  })
+  @ApiQuery({
+    name: 'limit',
+    type: 'string',
+    description: 'El limite maximo de ordenes',
+    example: '10',
+  })
+  @Get('orders/delivered')
+  async getAllOrdersDelivered(
+    @DafitiAuth() token: string = '',
+    @Query('limit') limit: string = '10',
+  ) {
+    return await this.dafitiService.ordersDelivered({ token, limit });
+  }
+
+  @ApiResponse({
+    status: 200,
+    description: RESPONSES.SUCCESS,
+    links: {},
+  })
+  @ApiResponse({
+    status: 401,
+    description: RESPONSES.UNAUTHORIZED,
+    links: {},
+  })
+  @ApiResponse({
+    status: 404,
+    description: RESPONSES.NOT_FOUND,
+    links: {},
+  })
+  @ApiResponse({
+    status: 408,
+    description: RESPONSES.LIMIT,
+    links: {},
+  })
+  @ApiParam({
+    name: 'id',
+    type: 'number',
+    description: 'El id de la orden',
+    example: 123435,
+  })
+  @Get('orders/:id')
+  async getOrderById(
+    @DafitiAuth() token: string = '',
+    @Param('id') id: number = 12345,
+  ) {
+    return await this.dafitiService.orderById({ token, id });
+  }
+
+  @ApiResponse({
+    status: 200,
+    description: RESPONSES.SUCCESS,
+    links: {},
+  })
+  @ApiResponse({
+    status: 401,
+    description: RESPONSES.UNAUTHORIZED,
+    links: {},
+  })
+  @ApiResponse({
+    status: 408,
+    description: RESPONSES.LIMIT,
+    links: {},
+  })
+  @Get('orders/faliure-reasones')
+  async getOrdersFailureReasones(@DafitiAuth() token: string = '') {
+    return await this.dafitiService.ordersFailureReasones({ token });
   }
 }
