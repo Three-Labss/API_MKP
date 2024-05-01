5a18986f02e14f60d96d76f0ce1692ec34955aca
KevnnardFedora <londonokevin9@gmail.com>
2024-04-30 21:15:57 -0500

    refactor(dafiti-service): dafitiService to add new methods and update existing ones

--- src/modules/dafiti/dafiti.service.ts
+++ src/modules/dafiti/dafiti.service.ts
@@ -1,31 +1,73 @@
 import { Injectable, RequestTimeoutException } from '@nestjs/common';
 import { HttpsService } from '../http/https.service';
 import { DAFITI, RESPONSES } from './constants';
+import { RequestResponse } from './types/response.types';
 
 @Injectable()
 export class DafitiService {
   constructor(private readonly httpsService: HttpsService) {}
 
-  async ordersPendingCount(
-    token: string,
-  ): Promise<{ totalOrdenesPendientes: number }> {
-    const ordersPending = await this.httpsService.getHttps({
+  async ordersCounters({ token }: RequestResponse) {
+    return await this.httpsService.getHttps({
       method: 'GET',
-      url: `${DAFITI.URL_API}/orders?limit=50&offset=0&section=status_pending`,
+      url: `${DAFITI.URL_API}/orders-counters`,
       token,
     });
-
-    return {
-      totalOrdenesPendientes: ordersPending.items.length,
-    };
   }
 
-  async ordersReadyToShip(token: string, limit: string) {
+  async ordersPending({ token, limit }: RequestResponse) {
+    if (Number(limit) > 50) throw new RequestTimeoutException(RESPONSES.LIMIT);
+
+    return await this.httpsService.getHttps({
+      method: 'GET',
+      url: `${DAFITI.URL_API}/orders?limit=${limit}&offset=0&section=status_pending&sortDir=desc`,
+      token,
+    });
+  }
+
+  async setOrdersReadyToShip({ token, pickup }: RequestResponse) {
+    return await this.httpsService.postHttps({
+      method: 'POST',
+      url: `${DAFITI.URL_API}/orders/statuses/set-to-ready-to-ship`,
+      token,
+      data: pickup,
+    });
+  }
+
+  async ordersReadyToShip({ token, limit }: RequestResponse) {
     if (Number(limit) > 50) throw new RequestTimeoutException(RESPONSES.LIMIT);
 
     return await this.httpsService.getHttps({
       method: 'GET',
       url: `${DAFITI.URL_API}/orders?limit=${limit}&offset=0&section=status_ready_to_ship&sortDir=asc`,
+      token,
+    });
+  }
+
+  async ordersShipped({ token, limit }: RequestResponse) {
+    if (Number(limit) > 50) throw new RequestTimeoutException(RESPONSES.LIMIT);
+
+    return await this.httpsService.getHttps({
+      method: 'GET',
+      url: `${DAFITI.URL_API}/orders?limit=${limit}&offset=0&section=status_shipped&sortDir=asc`,
+      token,
+    });
+  }
+
+  async ordersDelivered({ token, limit }: RequestResponse) {
+    if (Number(limit) > 50) throw new RequestTimeoutException(RESPONSES.LIMIT);
+
+    return await this.httpsService.getHttps({
+      method: 'GET',
+      url: `${DAFITI.URL_API}/orders?limit=${limit}&offset=0&section=status_delivered&sortDir=asc`,
+      token,
+    });
+  }
+
+  async orderById({ token, id }: RequestResponse) {
+    return await this.httpsService.getHttps({
+      method: 'GET',
+      url: `${DAFITI.URL_API}/orders/${id}`,
       token,
     });
   }
