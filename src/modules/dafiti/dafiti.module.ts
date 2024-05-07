b671e465a833e9d0f7275a5b0b578bd1e324aafd
KevnnardFedora <londonokevin9@gmail.com>
2024-05-07 09:49:09 -0500

    feat(dafiti): add PrismaModule to DafitiModule imports

--- src/modules/dafiti/dafiti.module.ts
+++ src/modules/dafiti/dafiti.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { PrismaModule } from 'src/configs/db/prisma.module';
 import { ClientModule } from '../client/client.module';
 import { HttpsModule } from '../http/https.module';
 import { DafitiController } from './dafiti.controller';
 import { DafitiService } from './dafiti.service';
 
 @Module({
-  imports: [ClientModule, HttpsModule],
+  imports: [PrismaModule, ClientModule, HttpsModule],
   controllers: [DafitiController],
   providers: [DafitiService],
 })
