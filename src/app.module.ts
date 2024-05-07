96343cf16d709f2e47d936d05be28795ade7c615
KevnnardFedora <londonokevin9@gmail.com>
2024-05-07 17:47:21 -0500

    feat: add order entry module to the application

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './configs/auth/auth.module';
@@ -8,14 +10,20 @@ import { PrismaModule } from './configs/db/prisma.module';
 import { ClientModule } from './modules/client/client.module';
 import { DafitiModule } from './modules/dafiti/dafiti.module';
 import { HttpsModule } from './modules/http/https.module';
+import { OrderEntryModule } from './modules/orderEntry/orderEntry.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     EventEmitterModule.forRoot(),
-    PrismaModule,
+    ServeStaticModule.forRoot({
+      serveRoot: '/app',
+      rootPath: join(__dirname, '..', 'app'),
+    }),
     HttpsModule,
     AuthModule,
+    PrismaModule,
+    OrderEntryModule,
     ClientModule,
     DafitiModule,
   ],
