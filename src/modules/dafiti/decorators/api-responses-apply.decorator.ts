4c063ac00d472961595d0e4325e3301d52cfe4a8
KevnnardFedora <londonokevin9@gmail.com>
2024-05-07 23:42:07 -0500

    feat: add ApiResponses decorator to handle API response descriptions

---
+++ src/modules/dafiti/decorators/api-responses-apply.decorator.ts
@@ -0,0 +1,43 @@
+import { applyDecorators } from '@nestjs/common';
+import { ApiResponse } from '@nestjs/swagger';
+import { RESPONSES } from '../constants';
+
+export function ApiResponses() {
+  return applyDecorators(
+    ApiResponse({
+      status: 200,
+      description: RESPONSES.SUCCESS,
+      links: {},
+    }),
+    ApiResponse({
+      status: 201,
+      description: RESPONSES.CREATED,
+      links: {},
+    }),
+    ApiResponse({
+      status: 400,
+      description: RESPONSES.BAD_REQUEST,
+      links: {},
+    }),
+    ApiResponse({
+      status: 401,
+      description: RESPONSES.UNAUTHORIZED,
+      links: {},
+    }),
+    ApiResponse({
+      status: 404,
+      description: RESPONSES.NOT_FOUND,
+      links: {},
+    }),
+    ApiResponse({
+      status: 408,
+      description: RESPONSES.LIMIT,
+      links: {},
+    }),
+    ApiResponse({
+      status: 500,
+      description: RESPONSES.SERVICE_UNAVAILABLE,
+      links: {},
+    }),
+  );
+}
