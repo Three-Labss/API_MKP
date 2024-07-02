e371378becb9f92f60b2ebec61b9282fb72f3ed6
KevnnardFedora <londonokevin9@gmail.com>
2024-07-02 14:54:09 -0500

    feat(dafiti): update DafitiModule to use correct controller and service for products

--- src/modules/dafiti/dafiti.module.ts
+++ src/modules/dafiti/dafiti.module.ts
@@ -2,8 +2,8 @@ import { Module } from '@nestjs/common';
 import { ClientModule } from '../client/client.module';
 import { DafitiController } from './dafiti.controller';
 import { DafitiService } from './dafiti.service';
-import { DafitiProductsController } from './orders/dafiti-orders.controller';
-import { DafitiProductsService } from './orders/dafiti-orders.service';
+import { DafitiProductsController } from './products/dafiti-products.controller';
+import { DafitiProductsService } from './products/dafiti-products.service';
 
 @Module({
   imports: [ClientModule],
