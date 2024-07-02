dd3823a8553027e829deb385664b35bf76bdd10b
KevnnardFedora <londonokevin9@gmail.com>
2024-07-02 14:51:21 -0500

    feat(dafiti): add DafitiProductsController and DafitiProductsService for managing

--- src/modules/dafiti/dafiti.module.ts
+++ src/modules/dafiti/dafiti.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { ClientModule } from '../client/client.module';
 import { DafitiController } from './dafiti.controller';
 import { DafitiService } from './dafiti.service';
+import { DafitiProductsController } from './orders/dafiti-orders.controller';
+import { DafitiProductsService } from './orders/dafiti-orders.service';
 
 @Module({
   imports: [ClientModule],
-  controllers: [DafitiController],
-  providers: [DafitiService],
+  controllers: [DafitiController, DafitiProductsController],
+  providers: [DafitiService, DafitiProductsService],
 })
 export class DafitiModule {}
